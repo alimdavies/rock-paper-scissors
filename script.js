080ffc2bcc00a3ce88418f4230432ef2194d0d7a
alimdavies <66089856+alimdavies@users.noreply.github.com>
2025-12-23 00:24:14 +0100

    replaced all console.log with confirm()

--- script.js
+++ script.js
@@ -34,14 +34,14 @@ function playRound(humanChoice, computerChoice) {
         case "rock":
             switch (computerChoice) {
                 case "rock":
-                    console.log("No one wins")
+                    confirm("No one wins")
                     break
                 case "paper":
-                    console.log("You lost")
+                    confirm("You lost")
                     computerScore++
                     break
                 case "scissors":
-                    console.log("You won")
+                    confirm("You won")
                     humanScore++
                     break
                 default:
@@ -51,14 +51,14 @@ function playRound(humanChoice, computerChoice) {
         case "paper":
             switch (computerChoice) {
                 case "rock":
-                    console.log("You won")
+                    confirm("You won")
                     humanScore++
                     break
                 case "paper":
-                    console.log("No one wins")
+                    confirm("No one wins")
                     break
                 case "scissors":
-                    console.log("You lost")
+                    confirm("You lost")
                     computerScore++
                 default:
                     break
@@ -67,15 +67,15 @@ function playRound(humanChoice, computerChoice) {
         case "scissors":
             switch (computerChoice) {
                 case "rock":
-                    console.log("You lost")
+                    confirm("You lost")
                     computerScore++
                     break
                 case "paper":
-                    console.log("You won")
+                    confirm("You won")
                     humanScore++
                     break
                 case "scissors":
-                    console.log("No one wins")
+                    confirm("No one wins")
                 default:
                     break
             }
@@ -85,14 +85,14 @@ function playRound(humanChoice, computerChoice) {
 }
 
 function playGame() {
-    console.log("Game started. The first one to win 5 rounds wins the game.")
+    confirm("Game started. The first one to win 5 rounds wins the game.")
     while(computerScore < 5 && humanScore < 5) {
         playRound(getHumanChoice(), getComputerChoice())
     }
     if(humanScore == 5) {
-        console.log("You won the game!")
+        confirm("You won the game!")
     } else if (computerScore == 5) {
-        console.log("You lost the game.")
+        confirm("You lost the game.")
     }
 }
 
