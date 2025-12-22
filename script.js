5c2e363e02a318732594979a9a08d43c6df0bd46
alimdavies <66089856+alimdavies@users.noreply.github.com>
2025-12-22 23:51:31 +0100

    completed playRound function

--- script.js
+++ script.js
@@ -1,1 +1,73 @@
-console.log('hello world')
+let humanScore = 0
+let computerScore = 0
+
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3) + 1
+    switch (choice) {
+        case 1:
+            return "rock"
+            break
+        case 2:
+            return "paper"
+            break
+        case 3:
+            return "scissors"
+            break
+    }
+}
+
+function getHumanChoice() {
+    return prompt("Rock, paper, scissors. Choose:")
+}
+
+
+function playRound(humanChoice, computerChoice) {
+    switch(humanChoice) {
+        case "rock":
+            switch (computerChoice) {
+                case "rock":
+                    console.log("No one wins")
+                    break
+                case "paper":
+                    console.log("You lost")
+                    break
+                case "scissors":
+                    console.log("You won")
+                    break
+                default:
+                    break
+            }
+            break
+        case "paper":
+            switch (computerChoice) {
+                case "rock":
+                    console.log("You won")
+                    break
+                case "paper":
+                    console.log("No one wins")
+                    break
+                case "scissors":
+                    console.log("You lost")
+                default:
+                    break
+            }
+            break
+        case "scissors":
+            switch (computerChoice) {
+                case "rock":
+                    console.log("You lost")
+                    break
+                case "paper":
+                    console.log("You won")
+                    break
+                case "scissors":
+                    console.log("No one wins")
+                default:
+                    break
+            }
+        default:
+            break
+    }
+}
+
+playRound(getHumanChoice(), getComputerChoice())
